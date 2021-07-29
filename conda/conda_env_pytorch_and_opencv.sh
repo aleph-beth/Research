981faced567c662480b0411ef3732a91eef2b342
Grégory <gregory.lagasse@aleph-beth.net>
2021-07-29 21:38:56 +0200

    Add in conda env pytorch and opencv, the package CUDA for julia

--- conda/conda_env_pytorch_and_opencv.sh
+++ conda/conda_env_pytorch_and_opencv.sh
@@ -1,7 +1,7 @@
-bash miniconda3/bin/conda init
-bash 
-conda activate Research
-
 # Pytoch && OpenCV
 conda install -y pytorch torchvision torchaudio cpuonly -c pytorch
 conda install -y -c conda-forge opencv
+
+pip install scikit-image
+
+julia -E 'using Pkg; Pkg.add("CUDA")'
