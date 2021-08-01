1f4a1d797fc462928679d8f46048c4bf408a8f54
Grégory <gregory.lagasse@aleph-beth.net>
2021-08-01 12:29:28 +0200

    Feature GPU/CPU for Pytorch and Julia CUDA

--- conda/conda_env_pytorch_and_opencv.sh
+++ conda/conda_env_pytorch_and_opencv.sh
@@ -1,8 +1,13 @@
 # Pytoch && OpenCV
-conda install -y pytorch torchvision torchaudio cpuonly -c pytorch
-conda install -y cudatoolkit=10.2 -c pytorch
+
+conda env list
+
+if $1 == "GPU"
+	conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
+	julia -E 'using Pkg; Pkg.add("CUDA")'
+
+if $1 == "CPU"
+	conda install pytorch torchvision torchaudio cpuonly -c pytorch
+
 conda install -y -c conda-forge opencv
-
 pip install scikit-image
-
-julia -E 'using Pkg; Pkg.add("CUDA")'
