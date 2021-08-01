8e02b2688d615d66056bae637ee9d265346e810f
Grégory <gregory.lagasse@aleph-beth.net>
2021-08-01 12:30:28 +0200

    Fix and if in bash for if

--- conda/conda_env_pytorch_and_opencv.sh
+++ conda/conda_env_pytorch_and_opencv.sh
@@ -5,9 +5,11 @@ conda env list
 if $1 == "GPU"
 	conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
 	julia -E 'using Pkg; Pkg.add("CUDA")'
+fi
 
 if $1 == "CPU"
 	conda install pytorch torchvision torchaudio cpuonly -c pytorch
+fi
 
 conda install -y -c conda-forge opencv
 pip install scikit-image
